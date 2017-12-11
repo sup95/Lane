4ebb4ad9b9aef98de031c8cda2f244529c00ab5a
Supriya Srivatsa <Supriya.Srivatsa@xome.com>
2017-12-11 20:48:44 +0530

    Added notes side section and header and body fields.

---
+++ notes_menu.js
@@ -0,0 +1,9 @@
+function openNotesMenu() {
+    document.getElementById("notesSideNav").style.width = "400px";
+    document.getElementById("overflow_menu").style.visibility = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("notesSideNav").style.width = "0";
+    document.getElementById("overflow_menu").style.visibility = "visible";
+}
